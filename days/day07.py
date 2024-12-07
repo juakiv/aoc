3b3b241149b4fb14b676ba106dfae1d0cd634f5a
Jussi <juakiv@utu.fi>
2024-12-07 10:33:19 +0200

    day 7 done

---
+++ days/day07.py
@@ -0,0 +1,46 @@
+from typing import Callable
+
+from solution.base import SolutionBase
+
+
+class Solution(SolutionBase):
+    def __init__(self):
+        super().__init__()
+        self.data = self.load_data("day07")
+
+        self.results: dict[int, list[int]] = {}
+
+        for line in self.data:
+            result, expression = line.split(": ")
+            self.results[int(result)] = list(map(int, expression.split(" ")))
+
+
+        self.addition = lambda x, y: x + y
+        self.multiplication = lambda x, y: x * y
+        self.concatenation = lambda x, y: int(str(x) + str(y))
+
+    def sums(self, target: int, current: int, numbers: list[int], operators: list[Callable[[int, int], int]]) -> bool:
+        if len(numbers) == 0:
+            return target == current
+
+        return any(self.sums(target, operator(current, numbers[0]), numbers[1:], operators) for operator in operators)
+
+    def part1(self) -> int:
+        total_sum = 0
+
+        for target, numbers in self.results.items():
+            total_sum += target if self.sums(target, numbers[0], numbers[1:], [self.addition, self.multiplication]) else 0
+
+        return total_sum
+
+    def part2(self) -> int:
+        total_sum = 0
+
+        for target, numbers in self.results.items():
+            total_sum += target if self.sums(target, numbers[0], numbers[1:], [self.addition, self.multiplication, self.concatenation]) else 0
+
+        return total_sum
+
+
+if __name__ == '__main__':
+    Solution().run()
