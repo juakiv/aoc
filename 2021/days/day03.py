80ee626e305d6dc84cbdfaf2ae8b368a5911e808
Jussi <jussikivistoo@gmail.com>
2026-01-20 22:09:57 +0200

    clean up a bit

--- 2021/days/day03.py
+++ 2021/days/day03.py
@@ -25,8 +25,7 @@ class Solution(SolutionBase):
         return int("".join(most_common_bits), 2) * int("".join(least_common_bits), 2)
 
     def part2(self) -> int:
-        oxygen_generator_rating = 0
-        co2_scrubber_rating = 0
+        result = 1
 
         for rating_type in ["oxygen", "co2"]:
             filtered_data = self.data[:]
@@ -51,12 +50,9 @@ class Solution(SolutionBase):
                 filtered_data = [line for line in filtered_data if line[bit_position] == desired_bit]
                 bit_position += 1
 
-            if rating_type == "oxygen":
-                oxygen_generator_rating = int(filtered_data[0], 2)
-            else:
-                co2_scrubber_rating = int(filtered_data[0], 2)
+            result *= int(filtered_data[0], 2)
 
-        return oxygen_generator_rating * co2_scrubber_rating
+        return result
 
 
 if __name__ == '__main__':
